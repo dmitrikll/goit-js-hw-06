3ec527a9ab951a25373d0acc365428774e9447b4
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2022-07-10 21:32:03 +0300

    task#6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const userInput = document.querySelector("#validation-input");
+userInput.addEventListener("blur", onBlur);
+
+function onBlur(txt) {
+  const inputNew = txt.currentTarget;
+  const inputNewLength = Number(inputNew.dataset.length);
+    
+  if (inputNew.value.length === inputNewLength) {
+    inputNew.classList.remove("invalid");
+    inputNew.classList.add("valid");
+  } else {
+    inputNew.classList.remove("valid");
+    inputNew.classList.add("invalid");
+  }
+}
