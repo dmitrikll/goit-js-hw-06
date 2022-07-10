e4367a668a3ad1a36cf84812cc5bd5f7486a996e
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2022-07-10 17:23:12 +0300

    task#2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsElement = document.querySelector("#ingredients");
+
+console.log(ingredientsElement);
+console.log(ingredients);
+
+const listIngridients = ingredients.map((ingredient) => {
+  const listIngridient = document.createElement("li");
+  listIngridient.textContent = ingredient;
+  listIngridient.classList.add("item");
+  return listIngridient;
+});
+
+ingredientsElement.append(...listIngridients)
