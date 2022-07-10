3d19d1ed20363ec83b7f95ed90f8af2f17a936ea
Dmytro Halytskyi <ds.galytskyi@gmail.com>
2022-07-10 23:03:27 +0300

    task#8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const userForm = document.querySelector(".login-form");
 
+function checkSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please enter your Email and Password");
+  }
+
+  const result = { email: email.value, password: password.value };
+  console.log(result);
+  event.currentTarget.reset();
+}
+
+userForm.addEventListener("submit", checkSubmit);
